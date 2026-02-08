8817b6efdc9118c94c891cc1ddb61154cd77aac5
Developer <developer@example.com>
2026-02-08 06:36:27 +0500

    Fix distDir for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   output: 'export',
-  distDir: 'dist',
+  distDir: '.next',
   images: {
     unoptimized: true,
   },
